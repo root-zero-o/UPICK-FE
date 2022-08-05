c939aefdb7902636dce2f142315aa917144eafec
kvv(Kevin Junho Kim) <59503331+kevinkim910408@users.noreply.github.com>
2022-08-05 18:13:24 -0400

    🐛: 시험배포 했는데 mobile view 안되서 이것저것  try 중

--- lib/DeviceDetect.tsx
+++ lib/DeviceDetect.tsx
@@ -6,7 +6,11 @@ interface Children {
 
 function DeviceDetect({ children }: Children) {
   return isMobile ? (
-    <div className="relative w-full h-full infinityScroll">{children}</div>
+    <div className="w-screen h-screen ">
+      <div className="scrollbar-hide fixed border-2 w-[375px] h-[812px] right-1/2 top-1/2 translate-x-1/2 -translate-y-1/2 overflow-x-hidden">
+        {children}
+      </div>
+    </div>
   ) : (
     <>
       <div className="w-screen h-screen ">

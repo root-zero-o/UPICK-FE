118a6b28a618b2bbd3b6179f5d362779ef3ce12b
kvv(Kevin Junho Kim) <59503331+kevinkim910408@users.noreply.github.com>
2022-08-08 08:20:21 -0400

    🔥: renewal

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -23,52 +23,12 @@ const signin = () => {
         innerLeft="-6.4%"
         outerWidth="504px"
         outerHeight="504px"
-        outerTop="-28.57%"
+        outerTop="10%"
         outerLeft="-31.73%"
         showInnerDIv={true}
         showOuterDiv={true}
         rot={0}
         bgFlag={true}
-      />
-      {/* 반갑습니다&손바닥 div */}
-      <div className="relative flex items-center justify-start w-full z-9 ml-[40px] mt-[90px]">
-        <span className="text-white text-[26px] leading-[36px] z-50 tracking-wide">
-          반갑습니다
-        </span>
-        <div className="absolute left-[75px] bottom-[-45px] z-40">
-          <Image src={Hand} width="91.76px" height="101.93px" alt="" />
-        </div>
-      </div>
-      {/* 로그인 input div */}
-      <div className="w-[317px] h-[343px] bg-lightBlue/50 backdrop-blur-md border-[2px] border-opacity-[0.56] border-white rounded-[51px] shadow-sign-p-input z-10 mt-[10px] m-auto">
-        <SignHeader signInFlag={true} />
-        {/* 로그인 input */}
-        <div className="flex flex-col justify-start items-center w-full h-full mt-[40.5px]">
-          <SignInput
-            type="email"
-            placeholder="Email"
-            showButton={false}
-            src=""
-          />
-          <SignInput
-            type="password"
-            placeholder="Password"
-            showButton={true}
-            src={DisablePW}
-          />
-          <SignArrow />
-          {/* 아이디 비번 찾기 - link 작업해야함 */}
-          <SignInputBottom
-            textOne="이메일/비밀번호"
-            textTwo="를 잊으셨나요?"
-            link=""
-          />
-        </div>
-      </div>
-      <SignBottom
-        firstText="혹시 유픽 첫방문이라면?"
-        secondText="회원가입"
-        link="/signup"
       />
     </div>
   );

3ea4d1a0594f4b1d88bc41ba1221479d415f5890
kvv(Kevin Junho Kim) <59503331+kevinkim910408@users.noreply.github.com>
2022-09-02 20:50:48 -0400

    💄 : Styling of TOS

--- pages/tos.tsx
+++ pages/tos.tsx
@@ -3,13 +3,42 @@ import useClickRoute from "hooks/useClickRoute";
 const Tos = () => {
   const onLink = useClickRoute({ link: "/signupdetail" });
   return (
-    <div className="h-screen flex flex-col items-center justify-between">
-      <div className="flex w-full h-[208px] bg-blue1 rounded-b-[101px]">
-        <span className="text-white text-[26px] font-bold ml-[40px] mt-[50px]">
+    <div className="relative h-screen flex flex-col items-center justify-between">
+      <div className="relative flex w-full h-[208px] bg-sign-p-one rounded-b-[101px] overflow-hidden">
+        <div className="absolute w-[440px] h-[415px] rounded-full bg-sign-p-two top-[-184px] left-[-93px] z-10" />
+        <div className="absolute w-[504px] h-[415px] rounded-full bg-sign-p-two top-[82px] left-[6px]" />
+        <span className="text-white text-[26px] font-bold ml-[40px] mt-[50px] z-20">
           이용약관
         </span>
       </div>
-      <div>이용약관 텍스트 들어올곳</div>
+      <div className="overflow-scroll max-h-[500px] w-[90%]">
+        <span>
+          이용약관 텍스트 들어올곳 스크롤 테스트||| 이용약관 텍스트
+          들어올곳이용약관 텍스트 들어올곳 ||| 이용약관 텍스트 들어올곳이용약관
+          텍스트 들어올곳 ||| 이용약관 텍스트 들어올곳이용약관 텍스트 들어올곳
+          ||| 이용약관 텍스트 들어올곳이용약관 텍스트 들어올곳 ||| 이용약관
+          텍스트 들어올곳이용약관 텍스트 들어올곳 ||| 이용약관 텍스트
+          들어올곳이용약관 텍스트 들어올곳 ||| 이용약관 텍스트 들어올곳이용약관
+          텍스트 들어올곳 ||| 이용약관 텍스트 들어올곳이용약관 텍스트 들어올곳
+          ||| 이용약관 텍스트 들어올곳이용약관 텍스트 들어올곳 ||| 이용약관
+          텍스트 들어올곳 스크롤 테스트||| 이용약관 텍스트 들어올곳이용약관
+          텍스트 들어올곳 ||| 이용약관 텍스트 들어올곳이용약관 텍스트 들어올곳
+          ||| 이용약관 텍스트 들어올곳이용약관 텍스트 들어올곳 ||| 이용약관
+          텍스트 들어올곳이용약관 텍스트 들어올곳 ||| 이용약관 텍스트
+          들어올곳이용약관 텍스트 들어올곳 ||| 이용약관 텍스트 들어올곳이용약관
+          텍스트 들어올곳 ||| 이용약관 텍스트 들어올곳이용약관 텍스트 들어올곳
+          ||| 이용약관 텍스트 들어올곳이용약관 텍스트 들어올곳 ||| 이용약관
+          텍스트 들어올곳이용약관 텍스트 들어올곳 ||| 이용약관 이용약관 텍스트
+          들어올곳 스크롤 테스트||| 이용약관 텍스트 들어올곳이용약관 텍스트
+          들어올곳 ||| 이용약관 텍스트 들어올곳이용약관 텍스트 들어올곳 |||
+          이용약관 텍스트 들어올곳이용약관 텍스트 들어올곳 ||| 이용약관 텍스트
+          들어올곳이용약관 텍스트 들어올곳 ||| 이용약관 텍스트 들어올곳이용약관
+          텍스트 들어올곳 ||| 이용약관 텍스트 들어올곳이용약관 텍스트 들어올곳
+          ||| 이용약관 텍스트 들어올곳이용약관 텍스트 들어올곳 ||| 이용약관
+          텍스트 들어올곳이용약관 텍스트 들어올곳 ||| 이용약관 텍스트
+          들어올곳이용약관 텍스트 들어올곳 ||| 이용약관
+        </span>
+      </div>
       <button className="confirmBtn" onClick={onLink}>
         모두 확인
       </button>

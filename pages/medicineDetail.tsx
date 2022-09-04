9e200adad78c2a7d9a238825fd5ee255e49c5e06
kvv(Kevin Junho Kim) <59503331+kevinkim910408@users.noreply.github.com>
2022-09-03 22:08:32 -0400

    💄 final view for medicine page

--- pages/medicineDetail.tsx
+++ pages/medicineDetail.tsx
@@ -4,6 +4,8 @@ import BackBtn from "components/BackBtn";
 import useClickRoute from "hooks/useClickRoute";
 import pathGray from "../assets/images/icons/PathGray.svg";
 import heart_line from "../assets/images/icons/heart-line.svg";
+import star_fill from "../assets/images/icons/star-fill.svg";
+import star_empty from "../assets/images/icons/star-emp.svg";
 
 const MedicineDetail = () => {
   const onLink = useClickRoute({ link: "/alert" });
@@ -93,6 +95,56 @@ const MedicineDetail = () => {
             </div>
           </div>
           {/* 가로 스크롤 후기? */}
+          <div className="w-[410px] flex mt-[19px] overflow-visible">
+            <div className="w-[122px] rounded-[13px] border-solid border-2 border-blue3 mr-[16px] px-[16px]">
+              <div className="text-darkblue1 font-extrabold text-[13px] mt-[19px]">
+                한성준 약사
+              </div>
+              <div className="mt-[13px] text-[12px] text-coolgray4 font-semibold">
+                착한 가격에 넉넉한 용량의 비타민C. 하루 1정씩, 240일치 (6개월
+                이상)
+              </div>
+              <div className="flex mt-[7px] gap-2 mb-[11px]">
+                <Image alt="" src={star_fill} />
+                <Image alt="" src={star_fill} />
+                <Image alt="" src={star_fill} />
+                <Image alt="" src={star_fill} />
+                <Image alt="" src={star_empty} />
+              </div>
+            </div>
+            <div className="w-[122px] rounded-[13px] border-solid border-2 border-blue3 mr-[16px] px-[16px]">
+              <div className="text-darkblue1 font-extrabold text-[13px] mt-[19px]">
+                한성준 약사
+              </div>
+              <div className="mt-[13px] text-[12px] text-coolgray4 font-semibold">
+                착한 가격에 넉넉한 용량의 비타민C. 하루 1정씩, 240일치 (6개월
+                이상)
+              </div>
+              <div className="flex mt-[7px] gap-2 mb-[11px]">
+                <Image alt="" src={star_fill} />
+                <Image alt="" src={star_fill} />
+                <Image alt="" src={star_fill} />
+                <Image alt="" src={star_fill} />
+                <Image alt="" src={star_empty} />
+              </div>
+            </div>
+            <div className="w-[122px] rounded-[13px] border-solid border-2 border-blue3 mr-[16px] px-[16px]">
+              <div className="text-darkblue1 font-extrabold text-[13px] mt-[19px]">
+                한성준 약사
+              </div>
+              <div className="mt-[13px] text-[12px] text-coolgray4 font-semibold">
+                착한 가격에 넉넉한 용량의 비타민C. 하루 1정씩, 240일치 (6개월
+                이상)
+              </div>
+              <div className="flex mt-[7px] gap-2 mb-[11px]">
+                <Image alt="" src={star_fill} />
+                <Image alt="" src={star_fill} />
+                <Image alt="" src={star_fill} />
+                <Image alt="" src={star_fill} />
+                <Image alt="" src={star_empty} />
+              </div>
+            </div>
+          </div>
           {/* 추천 태그 */}
         </div>
 

503704680fe6e727aac919071173db9b12d14173
영근 <rootzero17@gmail.com>
2022-11-08 00:01:38 +0900

    💄 add keyword Chip component

---
+++ components/KeywordChip.tsx
@@ -0,0 +1,20 @@
+const KeywordChip = ({
+  keyword,
+  style,
+}: {
+  keyword: string;
+  style?: string;
+}) => {
+  const customizingStyle = (style?: string) => {
+    const defaultStyle =
+      "text-sm text-white font-light pt-[2px] pb-[3px]  bg-coolgray3 px-1 rounded-[5px]";
+
+    if (!style) return;
+    const customStyle = defaultStyle.concat(" ", style);
+    return customStyle;
+  };
+
+  return <span className={customizingStyle(style)}>{keyword}</span>;
+};
+
+export default KeywordChip;
--- pages/article/[id].tsx
+++ pages/article/[id].tsx
@@ -1,3 +1,4 @@
+import KeywordChip from "components/KeywordChip";
 import Layout from "components/Layout";
 import { sampleArticleData } from "lib/sampleData";
 
@@ -16,8 +17,22 @@ const Article = () => {
           {article.author} 약사
         </span>
         <span className="text-sm font-light text-coolgray3 ml-5">
-          {article.author} 약국
+          {article.pharmacy} 약국
         </span>
+        <div className="pt-3 pb-2 border-b-[1px] border-coolgray3">
+          {article.title.map((v, i) => {
+            return (
+              <div key={i} className="text-2xl font-[700] text-darkblue1">
+                {v}
+              </div>
+            );
+          })}
+        </div>
+        <div className="mt-2">
+          {article.keyword.map((v, i) => {
+            return <KeywordChip key={i} keyword={v} style="mr-2" />;
+          })}
+        </div>
       </div>
     </Layout>
   );

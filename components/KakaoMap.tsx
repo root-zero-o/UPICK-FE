5e8e5a8da960eb1e9fbc3a1d59f21e6ba85762f9
영근 <rootzero17@gmail.com>
2022-11-17 00:50:55 +0900

    fix : 렌더링 시 지도 노출

--- components/KakaoMap.tsx
+++ components/KakaoMap.tsx
@@ -1,6 +1,6 @@
 import useGetLocation from "hooks/useGetLocation";
 import Script from "next/script";
-import React, { ReactElement, useCallback, useRef } from "react";
+import React, { ReactElement, useCallback, useEffect, useRef } from "react";
 
 const NEXT_PUBLIC_KAKAOMAP_KEY = process.env.NEXT_PUBLIC_KAKAOMAP_KEY;
 
@@ -17,11 +17,14 @@ const KakaoMap = (): ReactElement | null => {
       });
     }
   }, []);
+
+  useEffect(() => {
+    window.kakao?.maps.load(initMap);
+  }, []);
   return (
     <>
       <Script
         src={`https://dapi.kakao.com/v2/maps/sdk.js?appkey=${NEXT_PUBLIC_KAKAOMAP_KEY}&autoload=false`}
-        onLoad={() => window.kakao.maps.load(initMap)}
       />
       <div
         ref={containerRef}
